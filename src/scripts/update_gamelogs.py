54046361b12556887c532b226b4061f78eb2b8eb
chrislesante <chrislesante@gmail.com>
2024-11-16 21:13:00 -0500

    update gamelogs

---
+++ src/scripts/update_gamelogs.py
@@ -0,0 +1,165 @@
+from nba_api.stats.endpoints import CommonPlayerInfo
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playergamelog
+from datetime import date
+from utility.reference import sql
+import datetime
+import pandas as pd
+import time
+import os
+
+TODAY = date.today()
+ACTIVE_PLAYERS_DF = pd.DataFrame.from_records(players.get_active_players())
+MONTHS = {
+    "JAN": 1,
+    "FEB": 2,
+    "MAR": 3,
+    "APR": 4,
+    "MAY": 5,
+    "JUN": 6,
+    "JUL": 7,
+    "AUG": 8,
+    "SEP": 9,
+    "OCT": 10,
+    "NOV": 11,
+    "DEC": 12,
+}
+
+def get_current_gamelogs():
+    print("\nGrabbing current gamelogs...")
+    return sql.convert_sql_to_df(table_name="player_gamelogs", schema="gamelogs")
+
+
+def find_latest_game_date(current_gamelog_df):
+    print("\nFinding last gamedate...")
+
+    last_date = pd.to_datetime(
+        current_gamelog_df["GAME_DATE"].max()
+    ).date() + datetime.timedelta(days=1)
+    last_date = str(last_date)
+    convert = last_date.split("-")
+    year = convert[0]
+    month = convert[1]
+    day = convert[2]
+
+    return month + "/" + day + "/" + year
+
+
+def get_new_logs(last_game_date):
+    print("\nGrabbing new gamelogs...")
+
+    new_game_logs = []
+
+    player_dict = players.get_active_players()
+
+    for x in player_dict:
+        print(f"\n\tGrabbing gamelogs for player {x['id']}")
+        new_game_logs.append(
+            playergamelog.PlayerGameLog(
+                player_id=x["id"], date_from_nullable=last_game_date
+            )
+        )
+        time.sleep(0.200)
+
+    return new_game_logs
+
+
+def convert_new_logs_to_df(new_logs):
+    headers = new_logs[0].get_dict()["resultSets"][0]["headers"]
+    new_logs_df = pd.DataFrame(columns=headers)
+    for x in new_logs:
+        for n in x.get_dict()["resultSets"][0]["rowSet"]:
+            new_logs_df.loc[len(new_logs_df)] = n
+
+    new_logs_df["TEAM"] = new_logs_df["MATCHUP"].apply(get_team)
+    new_logs_df["OPPONENT"] = new_logs_df["MATCHUP"].apply(get_opponent)
+    new_logs_df["HOME/AWAY"] = new_logs_df["MATCHUP"].apply(get_home_away)
+
+    standard_date = lambda x: (
+    str(
+        x.split(" ")[2]
+        + "-"
+        + str(MONTHS[x.split(" ")[0]])
+        + "-"
+        + x.split(" ")[1].rstrip(",")
+    )
+    if x.split(" ")[0] in MONTHS
+    else x
+)
+
+    new_logs_df['GAME_DATE'] = new_logs_df.GAME_DATE.apply(standard_date)
+    new_logs_df['GAME_DATE'] = pd.to_datetime(new_logs_df["GAME_DATE"])
+
+    new_logs_df = new_logs_df.merge(
+        ACTIVE_PLAYERS_DF, how="inner", left_on="Player_ID", right_on="id"
+    ).drop(columns=["id", "first_name", "last_name"])
+
+    new_logs_df.rename({'full_name':'player_name'},inplace=True)
+    
+    new_logs_df.sort_values(by=['GAME_DATE','TEAM'],ascending=False,inplace=True)
+
+    new_logs_df = new_logs_df.drop_duplicates().reset_index(drop=True)
+
+    return new_logs_df
+
+
+def clean_matchup_column(matchup):
+    matchup = matchup.split()
+    team = matchup[0]
+    opponent = matchup[-1]
+    if matchup[1] == "@":
+        home_away = "AWAY"
+    else:
+        home_away = "HOME"
+
+    return team, opponent, home_away
+
+
+def get_team(matchup):
+    team, opponent, home_away = clean_matchup_column(matchup)
+    return team
+
+
+def get_opponent(matchup):
+    team, opponent, home_away = clean_matchup_column(matchup)
+    return opponent
+
+
+def get_home_away(matchup):
+    team, opponent, home_away = clean_matchup_column(matchup)
+    return home_away
+
+def export_flatfiles(current_gamelog_df: pd.DataFrame, new_logs_df: pd.DataFrame) -> None:
+    download_path = f"{sql.FLATFILE_PATH}previous_version/{TODAY}"
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    current_gamelog_df.to_csv(f"{download_path}/old_gamelogs.csv", index=False)
+
+    download_path = f"{sql.FLATFILE_PATH}added_logs/{TODAY}"
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    new_logs_df.to_csv(f"{download_path}/new_gamelogs.csv", index=False)
+
+    print(f"\nFlatfiles exported to {sql.FLATFILE_PATH}")
+
+def main():
+    current_gamelog_df = get_current_gamelogs()
+    latest_game_date = find_latest_game_date(current_gamelog_df)
+    new_logs = get_new_logs(latest_game_date)
+    new_logs_df = convert_new_logs_to_df(new_logs)
+
+    new_logs_df = new_logs_df[current_gamelog_df.columns]
+
+    updated_logs = pd.concat([current_gamelog_df, new_logs_df])
+
+    updated_logs.drop_duplicates(inplace=True)
+
+    export_flatfiles(current_gamelog_df, new_logs_df)
+
+    sql.export_df_to_sql(df=updated_logs, table_name="player_gamelogs", schema="gamelogs", behavior="replace")
+
+
+if __name__ == "__main__":
+    main()
--- src/utility/reference/sql.py
+++ src/utility/reference/sql.py
@@ -2,7 +2,9 @@ import psycopg as ps
 import pandas as pd
 from sqlalchemy import create_engine
 import getpass
+import os
 
+FLATFILE_PATH = f"{os.environ['HOME']}/Desktop/nba_flatfiles/"
 USER = "postgres"
 PASSWORD = getpass.getpass("\nEnter SQL password: ")
 HOST = "localhost"
@@ -42,7 +44,7 @@ def export_df_to_sql(
                 valid = True
             else:
                 print("\nEnter a valid option.")
-
+    
     df.to_sql(
         name=table_name,
         con=get_connection(),
@@ -52,3 +54,15 @@ def export_df_to_sql(
     )
 
     print(f"\n{table_name} successfully imported into {schema}.")
+
+def convert_sql_to_df(table_name: str | None = None, schema: str | None = None, query: bool = False):
+    if (table_name == None) and (query == True):
+        table_name = input("\nEnter table name: ")
+
+    if (schema == None) and (query == True):
+        schema = input("\nEnter schema where table is present: ")
+
+    if query == False:
+        return pd.read_sql_table(table_name=table_name, con=get_connection(), schema=schema)
+    else:
+        return pd.read_sql(sql=query, con=get_connection())
