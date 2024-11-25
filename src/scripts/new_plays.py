12075bf305799ea66e60b0fcfca371cb0333e71c
chrislesante <chrislesante@gmail.com>
2024-11-25 00:33:09 -0500

    play by play

---
+++ src/scripts/new_plays.py
@@ -0,0 +1,44 @@
+from utility.reference import sql
+from nba_api.stats.endpoints import playbyplayv3 as pp
+import pandas as pd
+import time
+import json
+import requests
+
+def get_play_by_play_data():
+    logs = sql.convert_sql_to_df(table_name="player_gamelogs", schema="gamelogs")
+
+    game_ids = logs["Game_ID"].unique()
+
+    print("\nGrabbing play by play data...")
+    
+    for n, id_ in enumerate(game_ids):
+        for attempt in range(0, 5):
+            print(f"\n\tGetting playbyplay data for {id_}")
+            try:
+                try:
+                    play = pp.PlayByPlayV3(game_id=id_)
+                except IndexError:
+                    play = pp.PlayByPlayV3(game_id=f"00{id_}")
+                df = pd.DataFrame(columns=play.play_by_play.get_dict()["headers"], data=play.play_by_play.get_dict()["data"])
+                if n == 0:
+                    export = df.copy()
+                else:
+                    export = pd.concat([export, df])
+                time.sleep(0.3)
+                break
+            except json.decoder.JSONDecodeError:
+                print("\n\t\tError, trying again...")
+                time.sleep(0.5)
+            except requests.exceptions.ReadTimeout:
+                print("\n\t\tError, trying again...")
+                time.sleep(3)
+    
+    return export
+
+def main():
+    export = get_play_by_play_data()
+    sql.export_df_to_sql(df=export, table_name="play_by_play", schema="gamelogs", behavior="replace")
+
+if __name__ == "__main__":
+    main()
