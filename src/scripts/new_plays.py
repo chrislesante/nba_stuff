12a7a3da8384def4b48bbb15a04e61fe7a31a6b1
chrislesante <chrislesante@gmail.com>
2024-12-07 13:08:53 -0500

    update plays

--- src/scripts/new_plays.py
+++ src/scripts/new_plays.py
@@ -6,13 +6,21 @@ import json
 import requests
 
 def get_play_by_play_data():
+    print("\nLoading gamelogs df...")
     logs = sql.convert_sql_to_df(table_name="player_gamelogs", schema="gamelogs")
 
-    game_ids = logs["Game_ID"].unique()
+    print("\nLoading play by play df...")
+    play_by_play = sql.convert_sql_to_df(table_name="play_by_play", schema="gamelogs")
 
-    print("\nGrabbing play by play data...")
+    game_ids = logs["Game_ID"].unique()
+    play_by_play["gameId"] = play_by_play["gameId"].apply(lambda x: str(x).lstrip("00"))
+    play_by_play_games = play_by_play["gameId"].unique()
+
+    new_ids = [x for x in game_ids if x not in play_by_play_games]
+
+    print("\nGrabbing new play by play data...")
     
-    for n, id_ in enumerate(game_ids):
+    for n, id_ in enumerate(new_ids):
         for attempt in range(0, 5):
             print(f"\n\tGetting playbyplay data for {id_}")
             try:
@@ -20,11 +28,9 @@ def get_play_by_play_data():
                     play = pp.PlayByPlayV3(game_id=id_)
                 except IndexError:
                     play = pp.PlayByPlayV3(game_id=f"00{id_}")
+
                 df = pd.DataFrame(columns=play.play_by_play.get_dict()["headers"], data=play.play_by_play.get_dict()["data"])
-                if n == 0:
-                    export = df.copy()
-                else:
-                    export = pd.concat([export, df])
+                play_by_play = pd.concat([play_by_play, df])
                 time.sleep(0.3)
                 break
             except json.decoder.JSONDecodeError:
@@ -34,7 +40,7 @@ def get_play_by_play_data():
                 print("\n\t\tError, trying again...")
                 time.sleep(3)
     
-    return export
+    return play_by_play
 
 def main():
     export = get_play_by_play_data()
